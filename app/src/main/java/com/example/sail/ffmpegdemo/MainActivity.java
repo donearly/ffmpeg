7199a71866e3b52b08b9b3903a61eea73e8fbff7
douning <ning_dou@sina.com>
2017-06-13 21:48:07 +0800

    测试

--- app/src/main/java/com/example/sail/ffmpegdemo/MainActivity.java
+++ app/src/main/java/com/example/sail/ffmpegdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sail.ffmpegdemo;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +13,7 @@ import android.widget.Button;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
+    private static final int REQUESTCODE = 101 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,22 +22,42 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.print("clicked \n");
-             //   mDecode();
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        mDecode();
+                        test();
                     }
                 }).start();
             }
         });
     }
 
-    @SuppressLint("SdCardPath")
-    public void mDecode(){
-      //  @SuppressLint("SdCardPath") String input = new File("/sdcard/input.mp4").getAbsolutePath();
-     //   String output = new File(Environment.getExternalStorageDirectory(),"/sdcard/output_1280x720_yuv420p.yuv").getAbsolutePath();
-        VideoUtils.decode("/sdcard/input.mp4", "/sdcard/output_1280x720_yuv420p.yuv");
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUESTCODE){
+            if (permissions[0].equals(Manifest.permission.WRITE_EXTERNAL_STORAGE ) && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                mDecode(Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"input.mp4",
+                        Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"output.yuv");
+            }
+        }
+    }
+
+    private void test() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            int check = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE );
+            if (check == PackageManager.PERMISSION_DENIED){
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE },REQUESTCODE);
+            }else{
+                mDecode(Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"input.mp4",
+                        Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"output.yuv");
+            }
+        }else{
+            mDecode(Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"input.mp4",
+                    Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"output.yuv");
+        }
+    }
+    public void mDecode(String input,String output){
+        VideoUtils.decode(input, output);
     }
 }
